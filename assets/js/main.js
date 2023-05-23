725f06b531b4ca52318663d81fab1c72f92e6f5d
Huy Duong <https://github.com/Dhuy0911>
2023-05-23 19:23:55 +0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,65 @@
+// LOADING
+// Khi trang web được tải hoàn chỉnh
+window.addEventListener("load", function () {
+  document.body.classList.add("loaded");
+});
+
+//    HEADER START
+const hamburger = $(".hamburger");
+const navMenu = $(".nav-menu-mobile");
+
+hamburger.on("click", () => {
+  hamburger.toggleClass("active");
+  navMenu.toggleClass("active");
+  if (navMenu.hasClass("active")) {
+    navMenu.css("display", "block");
+  } else {
+    navMenu.css("display", "none");
+  }
+});
+
+$(".nav-link").each(function () {
+  $(this).on("click", function () {
+    hamburger.removeClass("active");
+    navMenu.removeClass("active");
+
+    navMenu.css("display", "none");
+  });
+});
+
+//    HEADER END
+
+// $(window).scroll(function () {
+//   if ($(window).scrollTop() > 100) {
+//     $(".sticky-header").addClass("sticky");
+//   } else {
+//     $(".sticky-header").removeClass("sticky");
+//   }
+// });
+// animate
+
+const items = document.querySelectorAll(".appear2");
+
+const active = function (entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("inview2");
+    } else {
+      entry.target.classList.remove("inview2");
+    }
+  });
+};
+const io2 = new IntersectionObserver(active);
+for (let i = 0; i < items.length; i++) {
+  io2.observe(items[i]);
+}
+
+window.addEventListener("load", function () {
+  var leftElement = document.querySelector(".left");
+  var rightElement = document.querySelector(".right");
+
+  leftElement.classList.add("animated-left");
+  rightElement.classList.add("animated-right");
+});
+
+AOS.init();
